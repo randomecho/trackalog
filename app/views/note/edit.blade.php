0c201d7d36c3e27d60784176e7536a457845059d
Soon Van <cog@randomecho.com>
2014-08-21 23:24:52 -0400

    Bee sting on number fields

--- app/views/note/edit.blade.php
+++ app/views/note/edit.blade.php
@@ -38,30 +38,35 @@
 
 {{ Form::label('description', 'Notes') }}
 {{ Form::textarea('description', $item->description) }}
-
 {{ Form::label('commits', 'Commits') }}
 {{ Form::textarea('commits', $item->commits) }}
-
 {{ Form::label('files', 'Files affected') }}
 {{ Form::textarea('files', $item->files) }}
-
+<div class="row">
+<div class="half">
 {{ Form::label('minutes', 'Minutes') }}
-{{ Form::text('minutes', $item->minutes) }}
-
+{{ Form::text('minutes', $item->minutes, array('class' => 'huge-field')) }}
+</div>
+<div class="half">
 {{ Form::label('cost', 'Cost') }}
-{{ Form::text('cost', $item->cost) }}
-
+{{ Form::text('cost', $item->cost, array('class' => 'huge-field')) }}
+</div>
+</div>
+<div class="row">
+<div class="third">
 {{ Form::label('reference', 'Reference') }}
 {{ Form::text('reference', $item->reference) }}
-
+</div>
+<div class="third">
 {{ Form::label('when_issued', 'Issued') }}
 {{ Form::text('when_issued', $item->when_issued) }}
-
+</div>
+<div class="third">
 {{ Form::label('when_paid', 'Paid on') }}
 {{ Form::text('when_paid', $item->when_paid) }}
+</div>
+</div>
 
 {{ Form::submit('Save') }}
-
 {{ Form::close() }}
-
 @stop
